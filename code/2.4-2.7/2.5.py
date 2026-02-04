e0b59b75fba08148542477f4d4e5d91f88a7ac20
yxh <yexinhao1@mails.gdut.edu.cn>
2026-02-04 12:23:41 +0800

    2.5 work commit

---
+++ code/2.4-2.7/2.5.py
@@ -0,0 +1,33 @@
+#2.5
+#learn
+
+sum = [i**2 for i in range(10)]
+print(sum)
+
+iterable = ["张三","李四"]
+students = list(map(lambda s:"".join(["QG_",s]),iterable))
+print(students)
+
+#practise
+
+raw_data = ["85", "92", "ERROR", "1050", "78", "WARNING", "99", "120"]
+
+def condition(s:str):
+    try:
+        #这里不用int(),s.isnumeric(),s.isdigit()
+        #是因为他们只能处理证书，而float可以处理整数，小数，更加全面
+        temp = float(s)
+        if temp >= 80:
+            return True
+        else:
+            return False
+    except:
+        return False
+num_data = [float(num) for num in raw_data if condition(num)]
+# 当然这里要用filter也可以
+# num_data = list(map(float,filter(condition,raw_data)))
+max_num = max(num_data)
+min_num = min(num_data)
+result = list(map(lambda x : (x-min_num)/(max_num-min_num),num_data))
+output = ["核心过载" if x > 0.8 else "运转正常" for x in result]
+print(output)
